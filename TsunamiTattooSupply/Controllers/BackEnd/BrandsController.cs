f6eb714541e8b002b37e9f5b9ff62456beb1a118
charbelabisaad <charbel.b.abisaad@gmail.com>
2025-11-23 22:54:50 +0200

    TsunamiTattooSupply2025-11-23_10-54

--- TsunamiTattooSupply/Controllers/BackEnd/BrandsController.cs
+++ TsunamiTattooSupply/Controllers/BackEnd/BrandsController.cs
@@ -1,12 +1,87 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TsunamiTattooSupply.Data;
+using TsunamiTattooSupply.DTO;
+using TsunamiTattooSupply.Models;
 
 namespace TsunamiTattooSupply.Controllers.BackEnd
 {
 	public class BrandsController : Controller
 	{
+
+		private readonly TsunamiDbContext _dbContext;
+		private readonly ILogger<BrandsController> _logger;
+		private readonly IWebHostEnvironment _env;
+
+		public BrandsController(TsunamiDbContext dbContext, ILogger<BrandsController> logger, IWebHostEnvironment env)
+		{
+			_dbContext = dbContext;
+			_logger = logger;
+			_env = env;
+		}
+
+
 		public IActionResult Index()
 		{
-			return View("~/Views/BackEnd/Brands/Index.cshtml");
+			
+				return View("~/Views/BackEnd/Brands/Index.cshtml");
 		}
+
+		[HttpGet] 
+		public IActionResult ListGetBrands()
+		{
+			try
+			{
+				var brands = GetBrands();
+				return Json(new { data = brands, success = true});
+			}
+			catch (Exception ex)
+			{
+
+				return Json(new
+				{
+					data = new List<Brand>(),
+					success = false,
+					message = "An unexpected error occurred while loading brands"
+				});
+
+			}
+		}
+
+		public List<BrandDto> GetBrands()
+		{
+			List<BrandDto> brands = new List<BrandDto>();
+
+			try
+			{
+ 
+				brands = _dbContext.Brands
+				.Include(b =>  b.Status)
+				.Select( b => new BrandDto
+					{
+						ID = b.ID,
+						Name = b.Name,
+						ShowHome = b.ShowHome,
+						Rank = b.Rank,
+						StatusID = b.Status.ID,
+						Status = b.Status.Description,
+						StatusColor = b.Status.Color
+					}
+				).ToList();
+			}
+			catch (Exception ex)
+			{
+
+				brands = new List<BrandDto>();
+				_logger.LogError(ex, "Fetch Brands [ERROR]");
+
+			}
+
+			return brands;
+			 
+		}
+
 	}
+
+
 }
---
+++ TsunamiTattooSupply/DTO/BrandDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using TsunamiTattooSupply.Models;
+
+namespace TsunamiTattooSupply.DTO
+{
+	public class BrandDto
+	{
+	 
+		public int ID { get; set; }
+		 
+		public string Name { get; set; }
+		 
+		public string? Summary { get; set; }
+		 
+		public string? Image { get; set; }
+		 
+		public bool ShowHome { get; set; } = true;
+		 
+		public int Rank { get; set; } = 0;
+ 
+		public string StatusID { get; set; }
+
+		public string Status {  get; set; }
+
+		public string StatusColor { get; set; }
+		 
+	}
+}
