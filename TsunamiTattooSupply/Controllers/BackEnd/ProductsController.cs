c65447a82d43bda22486e2d4c7e80486df81e464
charbelabisaad <charbel.b.abisaad@gmail.com>
2026-03-07 00:18:26 +0200

    TsunamiTattooSupply_2026-03-07_00-17

--- TsunamiTattooSupply/Controllers/BackEnd/ProductsController.cs
+++ TsunamiTattooSupply/Controllers/BackEnd/ProductsController.cs
@@ -1420,6 +1420,7 @@ namespace TsunamiTattooSupply.Controllers.BackEnd
 				}
 			}
 		}
+
 		private async Task SaveImages(int productId, int userId, DateTime now)
 		{
 			var allImages = _dbContext.ProductsImages
